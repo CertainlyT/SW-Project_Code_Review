84e477528a0f467e8a8bbafe6e76ba4493c3e0f5
jjolbo <jeongha3751@kookmin.ac.kr>
2017-09-06 17:46:38 -0700

    the final factorial code

--- week2/20171677_이정하_assignment2.py
+++ week2/20171677_이정하_assignment2.py
@@ -12,10 +12,11 @@ while True:
     num = int(input("Enter a number: "))
     if num == -1:
         break
-
-    if num == 0:
+    elif num == 0:
         print(str(num) + "! = " + "1")
+    elif num < 0:
+        print()
     else:
         for value in range(1, num + 1):
             result *= value
-        print(str(num) + "! = " + str(result))
+        print("%d != " %num, result)
