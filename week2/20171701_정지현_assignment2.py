57d4ad1e1475569a39d0afaf5f0e3383abd80240
CertainlyT <xcm1321@gmail.com>
2017-09-07 09:49:19 +0900

    solve print style

--- week2/20171701_정지현_assignment2.py
+++ week2/20171701_정지현_assignment2.py
@@ -1,9 +1,11 @@
 n = int(input())
 while n != -1:
+    fact = 1
     if n <= -2:
         print("Not identified")
-    fact = 1
-    for i in range(1, n + 1):
-        fact *= i
-    print("%d! =" % n, fact)
-    n = int(input())
+        n = int(input())
+    else:
+        for i in range(1, n + 1):
+            fact *= i
+        print("%d! =" % n, fact)
+        n = int(input())
