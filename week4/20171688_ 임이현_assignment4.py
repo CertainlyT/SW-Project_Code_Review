3d4812b88a7479f40c14b05eb7032832858d6b89
ehyeonni <e_hyeon98@kookmin.ac.kr>
2017-09-26 09:50:58 +0900

    팩토리알 주석처리
    .

--- week4/20171688_ 임이현_assignment4.py
+++ week4/20171688_ 임이현_assignment4.py
@@ -1,11 +1,13 @@
-def facto(n):
-    f = 1
-    for i in range (1,n+1) :
-        f *= i
-    return f
+#  *fectorial
 
-def combination(n, m):
-    return int(facto(n) / (facto(m) * facto(n - m)))
+# def facto(n):
+#     f = 1
+#     for i in range (1,n+1) :
+#         f *= i
+#     return f
+#
+# def combination(n, m):
+#     return int(facto(n) / (facto(m) * facto(n - m)))
 
 def calculate(n, m):
     if (m == 0 or n==m):
@@ -13,7 +15,7 @@ def calculate(n, m):
     elif n < m:
         return 0
     else:
-         return combination(n - 1, m) + combination(n - 1, m - 1)
+         return calculate(n - 1, m) + calculate(n - 1, m - 1)
 
 
 while (1):
