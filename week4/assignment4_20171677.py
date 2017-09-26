2c2a25d04795eff667af13bb4a8e8593acb8cf31
jjolbo <jeongha3751@kookmin.ac.kr>
2017-09-26 02:09:30 +0000

    modify file

---
+++ week4/assignment4_20171677.py
@@ -0,0 +1,29 @@
+# 20171677 이정하
+# assignment4 :조합으로 factorial 만들기
+
+def combination_recursive(n, r):
+    if n == 0 or r == 0:
+        return 1
+    elif n < r or n < 0 or r < 0:
+        return 0
+    elif n == r:
+        return 1
+    else:
+        return combination_recursive(n-1, r) + combination_recursive(n-1, r-1)
+
+
+while True:
+    try:
+        n = int(input("Enter n : "))
+
+        if n == -1:
+            break
+        m = int(input("Enter m : "))
+
+    except ValueError:
+        print("숫자를 입력해주세요")
+        continue
+
+    print("C(%d,%d) = %d" %(n, m, combination_recursive(n, m)))
+
+
