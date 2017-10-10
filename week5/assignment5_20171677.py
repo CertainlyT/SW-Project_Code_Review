8f26525c8793a1f3373fb090ac346a70ed808bec
Jihyeon Jeong <xcm1321@gmail.com>
2017-10-10 09:28:35 +0900

    Create assignment5_20171677.py

---
+++ week5/assignment5_20171677.py
@@ -0,0 +1,44 @@
+import time
+
+
+def iterfibo(nbr):
+    sum = 0
+    before = 0
+    after = 1
+    if nbr == 1 or nbr == 2:
+        return 1
+
+    elif nbr > 2:
+        for i in range(nbr - 1):
+            sum = before + after
+            before = after
+            after = sum
+
+    return sum
+
+
+def fibo(nbr):
+    if nbr == 1 or nbr == 2:
+        return 1
+    else:
+        return fibo(nbr - 1) + fibo(nbr - 2)
+
+
+while True:
+    nbr = int(input("Enter a number: "))
+
+    if nbr == -1:
+        break
+
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+
+    ts = time.time() - ts
+    print("IterFibo(%d)=%d, time %.6f" % (nbr, fibonumber, ts))
+
+    ts = time.time()
+    fibonumber = fibo(nbr)
+
+    ts = time.time() - ts
+    print("Fibo(%d)=%d, time %.6f" % (nbr, fibonumber, ts))
+    pass
