5b1c17828d7223144a68d7ee200a1936af8ece4e
Jihyeon Jeong <xcm1321@gmail.com>
2017-10-10 09:27:57 +0900

    Create assignment5_20171701.py

---
+++ week5/assignment5_20171701.py
@@ -0,0 +1,30 @@
+import time
+
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+
+def iterfibo(n):
+    a = 1
+    b = 1
+    for i in range(1,n+1):
+        if (i >2):
+            tmp = a
+            a = b
+            b = a + tmp
+    return b
+
+while True:
+    nbr = int (input("Enter a number: "))
+    if nbr == -1:
+        break
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print("IterFibo(%d) = %d, time %.6f" %(nbr, fibonumber, ts))
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print("Fibo(%d) = %d, time %.6f" %(nbr, fibonumber, ts))
