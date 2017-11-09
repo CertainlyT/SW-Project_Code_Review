f664b8c0293e1f1000b149f4cb50b1001a3760b1
CertainlyT <xcm1321@gmail.com>
2017-11-09 09:07:40 +0900

    remove unneeded files

--- week2/ddd.py
+++
@@ -1,9 +0,0 @@
-def solution(arr):
-    answer = []
-    i=0
-    for i in range(len(arr)):
-        while arr[i] != arr[i+1]:
-            if arr[i] != arr[i+1] :
-                arr.remove(arr[i+1])
-        i += 1    
-    return answer
